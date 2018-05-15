3039fd4691f4e099ad1458f20ca1a048c0e97a92
MAIOR\MATO <tommaso.magherini@maior.it>
2018-05-15 16:30:26 +0200

    Created a class for Oracle SQL server management

---
+++ OracleSQL/OracleSQL/OracleSQLServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+
+namespace OracleSQL
+{
+    class OracleSQLServer
+    {
+        public static string connectionString = "Data Source = 192.168.11.122:1521/DEVICARO; User Id = user_acm; Password = EBLA;";
+
+        public void ExecuteQuery(String sqlQuery)
+        {
+            try
+            {
+                OracleConnection connection = new OracleConnection(connectionString);
+                connection.Open();
+
+                OracleCommand cmd = new OracleCommand();
+                cmd.CommandText = sqlQuery;
+                cmd.Connection = connection;
+
+                int rowNumber = 1;
+                OracleDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    int fieldCount = dr.FieldCount;
+                    Console.Write(rowNumber++ + " ");
+                    for (int i = 0; i < fieldCount; i++)
+                    {
+                        Console.Write(dr[i] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+                connection.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
--- OracleSQL/OracleSQL/Program.cs
+++ OracleSQL/OracleSQL/Program.cs
@@ -5,40 +5,15 @@ using System.Web;
 using Oracle.DataAccess.Client;
 using System.Data;
 
-namespace OraWinApp
+namespace OracleSQL
 {
     static class Program
     {
         static void Main()
         {
-            try
-            {
-                string connectionString = "Data Source = 192.168.11.122:1521/DEVICARO; User Id = user_acm; Password = EBLA;";
-
-                OracleConnection connection = new OracleConnection(connectionString);
-                connection.Open();
-
-                OracleCommand cmd = new OracleCommand();
-                cmd.CommandText = "SELECT * from AEROPORTI_1";
-                cmd.Connection = connection;
-
-                OracleDataReader dr = cmd.ExecuteReader();
-                while (dr.Read())
-                {
-                    int fieldCount = dr.FieldCount;
-                    for (int i = 0; i < fieldCount; i++)
-                    {
-                        Console.Write(dr[i] + " ");
-                    }
-                    Console.WriteLine();
-                }
-                Console.ReadLine();
-                connection.Dispose();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            string sqlQuery = "SELECT* from ABILITAZIONE";
+            OracleSQLServer sqlServer = new OracleSQLServer();
+            sqlServer.ExecuteQuery(sqlQuery);
         }
     }
 }
