cfb4ca5c45e61675b3ab936f58a039165cfc1dfc
MAIOR\MATO <tommaso.magherini@maior.it>
2018-05-15 16:15:29 +0200

    Connection to the Oracle DB + results of a SQL query

--- OracleSQL/OracleSQL/Program.cs
+++ OracleSQL/OracleSQL/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using Oracle.DataAccess.Client;
+using System.Data;
 
-namespace OracleSQL
+namespace OraWinApp
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            try
+            {
+                string connectionString = "Data Source = 192.168.11.122:1521/DEVICARO; User Id = user_acm; Password = EBLA;";
+
+                OracleConnection connection = new OracleConnection(connectionString);
+                connection.Open();
+
+                OracleCommand cmd = new OracleCommand();
+                cmd.CommandText = "SELECT * from AEROPORTI_1";
+                cmd.Connection = connection;
+
+                OracleDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    int fieldCount = dr.FieldCount;
+                    for (int i = 0; i < fieldCount; i++)
+                    {
+                        Console.Write(dr[i] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+                connection.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
