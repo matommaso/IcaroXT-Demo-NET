f4223ae2f2d7211d9f26718f0e8d03f90256d049
MAIOR\MATO <tommaso.magherini@maior.it>
2018-05-16 16:42:49 +0200

    Created a connection between Oracle DB and Visual Studio Implemented Entity Framework

--- OracleSQL/OracleSQL/Program.cs
+++ OracleSQL/OracleSQL/Program.cs
@@ -11,9 +11,24 @@ namespace OracleSQL
     {
         static void Main()
         {
-            string sqlQuery = "SELECT* from ABILITAZIONE";
-            OracleSQLServer sqlServer = new OracleSQLServer();
-            sqlServer.ExecuteQuery(sqlQuery);
+            using (var db = new Entities())
+            {
+                foreach (ABILITAZIONE ab in db.ABILITAZIONEs)
+                {
+                    Console.Write(ab.SIGLA +" ");
+                    Console.Write(ab.MATR_UTENTE + " ");
+                    Console.Write(ab.DATA_INS + " ");
+                    Console.Write(ab.DATA_VAR + " ");
+                    Console.Write(ab.RICH_EQUIP_CONDOTTA + " ");
+                    Console.Write(ab.RICH_EQUIP_CABINA + " ");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadLine();
+            //string sqlQuery = "SELECT* from ABILITAZIONE";
+            //OracleSQLServer sqlServer = new OracleSQLServer();
+            //sqlServer.ExecuteQuery(sqlQuery);
         }
     }
 }
